162812380c0dceb900dcf37f64251afa73947717
wr <wr@wr-VirtualBox.(none)>
2013-03-16 17:30:32 +0800

    fcitx

--- Ubuntu-12.04-setup.sh
+++ Ubuntu-12.04-setup.sh
@@ -11,16 +11,18 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-2 -y 		# Sublime Text 2
 sudo add-apt-repository ppa:chromium-daily/stable -y			# Chromium
 sudo add-apt-repository ppa:webupd8team/themes -y 				# OS X主题
 sudo add-apt-repository ppa:ypubuntu-com/gtk3				#主题
+sudo add-apt-repository ppa:fcitx-team/nightly 				#fcitx+GooglePinyin
 sudo apt-get update -y
 
 echo "Install useful stuff"
 sudo apt-get install -y meld 				# meld diff tool（可视化的文件及目录比较工具）
 sudo apt-get install -y zsh 				# z shell
 sudo apt-get install -y chromium-browser 	# 安装Chromium浏览器
+sudo apt-get install fcitx fcitx-googlepinyin fcitx-module-cloudpinyin -y
 
 echo "Installing beautiful Unity themes"
 #sudo apt-get install gnome-cupertino-gtk-theme  #适合GTk3.6以后
-sudo apt-get install -y adwaita-cupertino-gtk-theme 	#适合Ubuntu12.04的Cupertino主题
+sudo apt-get install -y sudo apt-get install fcitx-module-cloudpinyinadwaita-cupertino-gtk-theme 	#适合Ubuntu12.04的Cupertino主题
 
 echo "Installing VLC Player"
 sudo apt-get install vlc vlc-plugin-pulse mozilla-plugin-vlc 
